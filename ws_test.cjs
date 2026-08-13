94a44a95b50849407be68c1964124e2b420a5232
idumand <iduman83516@gmail.com>
2026-08-13 20:13:04 +0300

    g

--- ws_test.cjs
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://fstream.binance.com/ws/!miniTicker@arr');
-ws.on('open', () => console.log('connected'));
-ws.on('message', (data) => {
-  console.log('msg', data.toString().substring(0, 300));
-  ws.close();
-});
-ws.on('error', (e) => console.log('error', e));
