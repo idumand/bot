3b394da077331b8026688bc01f8468b5a354510e
idumand <iduman83516@gmail.com>
2026-08-21 19:00:19 +0300

    h

--- fix_vars.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('server.ts', 'utf8');
-
-code = code.replace(/hasSupportWall/g, 'isBidWallStrong');
-code = code.replace(/hasResistanceWall/g, 'isAskWallStrong');
-
-fs.writeFileSync('server.ts', code);
--- ws_spot.cjs
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/!miniTicker@arr');
-ws.on('open', () => console.log('connected spot'));
-ws.on('message', (data) => {
-  console.log('msg spot', data.toString().substring(0, 300));
-  ws.close();
-});
-ws.on('error', (e) => console.log('error', e));
